e8bc71e3ac7f77f8a7d139e8d4bbeeb7a4d2ffed
kitada4010 <bnzz4010@yahoo.co.jp>
2018-07-12 23:51:07 +0900

    psthの手入力版完成

--- PSTH/beta1.c
+++ PSTH/beta1.c
@@ -2,12 +2,15 @@
 
 //#define FILE_NAME_MAX 256
 #define SECTION_DATA_MAX 256
+#define H 0.1
 
 int main(int argc, char *argv[]){
-  double data;
+  double data=0;
   double difference;
+  double hd;
+  double start;
   double section[SECTION_DATA_MAX] = {0};
-  int 
+  int spike=0;
   int i=0,j=0;
   int datafrag=0;
   FILE *input, *output;
@@ -16,28 +19,39 @@ int main(int argc, char *argv[]){
     printf("non file\n");
     return 0;
   }
-
+  
   printf("section data:");
   
   do{
     scanf("%lf", &section[i]);
   }while(section[i++] != 0);
   //printf("\n\n");
-
+  
+  
   output=fopen(argv[2],"w");
   
   for(j=0; j<i; j+=2){
-    do{
-      fscanf(input, "%lf", &data);
-      if((section[j] < data) && (section[j+1] > data)){
-	if(difference){
-	  fprintf(output,"%lf\n", data-difference);
+    start=section[j+1]-section[j];
+    start-=(int)start;
+    printf("%lf\n",start);
+    
+    while(section[j]+start>data)
+      fscanf(input,"%lf ",&data);
+    
+    for(hd=(section[j]+start); hd<=section[j+1]; hd+=H){
+      if(hd<data && (hd+H)>=data){
+	while(hd<data && (hd+H)>=data){
+	  spike++;
+	  fscanf(input,"%lf ",&data);
 	}
-	difference = data;
       }
-      else if(difference)
-	difference=0;
-    }while(section[j+1] > data);
+      else if(hd+H>data)
+	fscanf(input,"%lf ",&data);
+      fprintf(output,"%lf,%lf\n", hd, (spike*(1.0/H)));
+      spike=0;
+    }
+    
+    
     fprintf(output,"\n\n");
   }
   
