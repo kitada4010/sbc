dd9087b951c2de14e1f4a7d8d46aa53cd2d842c3
kitada4010 <bnzz4010@yahoo.co.jp>
2018-11-02 16:24:28 +0900

    解析中

---
+++ r/q-value-male.R
@@ -0,0 +1,184 @@
+####個体ごとの比較に変更####---------------------------------------------------------------------------
+##データの読み込み
+#male#---------------------------------------------
+#B26R
+maleB26Rbase=read.table("./male/B26Rbase.txt")
+maleB26R10=read.table("./male/B26R10.txt")
+maleB26R30=read.table("./male/B26R30.txt")
+#B38R
+maleB38Rbase=read.table("./male/B38Rbase.txt")
+maleB38R10=read.table("./male/B38R10.txt")
+maleB38R30=read.table("./male/B38R30.txt")
+
+
+####解析####--------------------
+
+##平均の導出##
+
+
+####開始########---------------------------------------------------------------------------------
+#B26R
+mean(maleB26Rbase[1:10,1])
+mean(maleB26R10[1:10,1])
+mean(maleB26R30[1:10,1])
+#B38R
+mean(maleB38Rbase[1:10,1])
+mean(maleB38R10[1:10,1])
+mean(maleB38R30[1:10,1])
+
+
+
+
+######個体ごとの比較########------------------------------------------------------------------------------------------
+##B26R##-------------------------
+B26RSMe <-(var(maleB26Rbase[1:10,1]) + var(maleB26R10[1:10,1]) + var(maleB26R30[1:10,1]))/3
+B26RSE <- sqrt(B26RSMe/10)
+B26Rq <- (mean(maleB26R10[1:10,1]) - mean(maleB26Rbase[1:10,1])) / B26RSE
+print(B26Rq)
+
+B26Rq1 <- abs(mean(maleB26Rbase[1:10,1]) - mean(maleB26R10[1:10,1])) / B26RSE
+B26Rq2 <- abs(mean(maleB26R10[1:10,1]) - mean(maleB26R30[1:10,1])) / B26RSE
+B26Rq3 <- abs(mean(maleB26R30[1:10,1]) - mean(maleB26Rbase[1:10,1])) / B26RSE
+print(B26Rq1)
+print(B26Rq2)
+print(B26Rq3)
+
+
+##B38R##-------------------------
+B38RSMe <-(var(maleB38Rbase[1:10,1]) + var(maleB38R10[1:10,1]) + var(maleB38R30[1:10,1]))/3
+B38RSE <- sqrt(B38RSMe/10)
+B38Rq <- (mean(maleB38Rbase[1:10,1]) - mean(maleB38R10[1:10,1])) / B38RSE
+print(B38Rq)
+
+B38Rq1 <- abs(mean(maleB38Rbase[1:10,1]) - mean(maleB38R10[1:10,1])) / B38RSE
+B38Rq2 <- abs(mean(maleB38R10[1:10,1]) - mean(maleB38R30[1:10,1])) / B38RSE
+B38Rq3 <- abs(mean(maleB38R30[1:10,1]) - mean(maleB38Rbase[1:10,1])) / B38RSE
+print(B38Rq1)
+print(B38Rq2)
+print(B38Rq3)
+
+
+
+#install.packages("gplots")
+#library(gplots)
+#plotmeans(x=list(datamalebase[,1],datamale10[,1],datamale30[,1]), data=x, connect=F, ylim=c(-180,180))
+
+#plotmeans(x=list(datamalebase[,1],datamale10[,1],datamale30[,1]))
+
+
+#####全体##########----------------------------------------------------------------
+##標準誤差の導出##
+
+SMe <-(var(maleB26Rbase[1:10,1]) + var(maleB26R10[1:10,1]) + var(maleB26R30[1:10,1]) 
+     + var(maleA30Rbase[1:10,1]) + var(maleA30R10[1:10,1]) + var(maleA30R30[1:10,1])
+     + var(maleB31Rbase[1:10,1]) + var(maleB31R10[1:10,1]) + var(maleB31R30[1:10,1])
+     + var(maleB32Lbase[1:10,1]) + var(maleB32L10[1:10,1]) + var(maleB32L30[1:10,1])
+     + var(maleB38Lbase[1:10,1]) + var(maleB38L10[1:10,1]) + var(maleB38L30[1:10,1])
+     + var(maleB38Rbase[1:10,1]) + var(maleB38R10[1:10,1]) + var(maleB38R30[1:10,1])
+     + var(maleB38Rbase[1:10,1]) + var(maleB38R10[1:10,1]) + var(maleB38R30[1:10,1]))/21
+
+SE <- sqrt(SMe/10)
+print(SE)
+
+##q値の導出##
+
+q <- (mean(maleB31R30[1:10,1]) - mean(maleB38Rbase[1:10,1])) / SE
+print(q)
+
+
+####終了########----------------------------------------------------------------------------------------------
+#####平均の導出####----------------------------------
+#B26R
+mean(maleB26Rbase[1:10,2])
+mean(maleB26R10[1:10,2])
+mean(maleB26R30[1:10,2])
+#B38R
+mean(maleB38Rbase[1:10,2])
+mean(maleB38R10[1:10,2])
+mean(maleB38R30[1:10,2])
+
+
+
+######個体ごとの比較########------------------------------------------------------------------------------------------
+##B26R##-------------------------
+B26RSMe <-(var(maleB26Rbase[1:10,2]) + var(maleB26R10[1:10,2]) + var(maleB26R30[1:10,2]))/3
+B26RSE <- sqrt(B26RSMe/10)
+B26Rq <- (mean(maleB26R10[1:10,2]) - mean(maleB26Rbase[1:10,2])) / B26RSE
+print(B26Rq)
+
+B26Rq1 <- abs(mean(maleB26Rbase[1:10,2]) - mean(maleB26R10[1:10,2])) / B26RSE
+B26Rq2 <- abs(mean(maleB26R10[1:10,2]) - mean(maleB26R30[1:10,2])) / B26RSE
+B26Rq3 <- abs(mean(maleB26R30[1:10,2]) - mean(maleB26Rbase[1:10,2])) / B26RSE
+print(B26Rq1)
+print(B26Rq2)
+print(B26Rq3)
+
+
+##B38R##-------------------------
+B38RSMe <-(var(maleB38Rbase[1:10,2]) + var(maleB38R10[1:10,2]) + var(maleB38R30[1:10,2]))/3
+B38RSE <- sqrt(B38RSMe/10)
+B38Rq <- (mean(maleB38Rbase[1:10,2]) - mean(maleB38R10[1:10,2])) / B38RSE
+print(B38Rq)
+
+B38Rq1 <- abs(mean(maleB38Rbase[1:10,2]) - mean(maleB38R10[1:10,2])) / B38RSE
+B38Rq2 <- abs(mean(maleB38R10[1:10,2]) - mean(maleB38R30[1:10,2])) / B38RSE
+B38Rq3 <- abs(mean(maleB38R30[1:10,2]) - mean(maleB38Rbase[1:10,2])) / B38RSE
+print(B38Rq1)
+print(B38Rq2)
+print(B38Rq3)
+
+
+
+#install.packages("gplots")
+#library(gplots)
+#plotmeans(x=list(datamalebase[,2],datamale10[,2],datamale30[,2]), data=x, connect=F, ylim=c(-180,180))
+
+#plotmeans(x=list(datamalebase[,2],datamale10[,2],datamale30[,2]))
+
+
+
+
+####中央########---------------------------------------------------------------------------------------------------------------
+#####平均の導出####----------------------------------
+#B26R
+mean(maleB26Rbase[1:10,3])
+mean(maleB26R10[1:10,3])
+mean(maleB26R30[1:10,3])
+#B38R
+mean(maleB38Rbase[1:10,3])
+mean(maleB38R10[1:10,3])
+mean(maleB38R30[1:10,3])
+
+
+
+######個体ごとの比較########------------------------------------------------------------------------------------------
+##B26R##-------------------------
+B26RSMe <-(var(maleB26Rbase[1:10,3]) + var(maleB26R10[1:10,3]) + var(maleB26R30[1:10,3]))/3
+B26RSE <- sqrt(B26RSMe/10)
+B26Rq <- (mean(maleB26R10[1:10,3]) - mean(maleB26Rbase[1:10,3])) / B26RSE
+print(B26Rq)
+
+B26Rq1 <- abs(mean(maleB26Rbase[1:10,3]) - mean(maleB26R10[1:10,3])) / B26RSE
+B26Rq2 <- abs(mean(maleB26R10[1:10,3]) - mean(maleB26R30[1:10,3])) / B26RSE
+B26Rq3 <- abs(mean(maleB26R30[1:10,3]) - mean(maleB26Rbase[1:10,3])) / B26RSE
+print(B26Rq1)
+print(B26Rq2)
+print(B26Rq3)
+
+
+##B38R##-------------------------
+B38RSMe <-(var(maleB38Rbase[1:10,3]) + var(maleB38R10[1:10,3]) + var(maleB38R30[1:10,3]))/3
+B38RSE <- sqrt(B38RSMe/10)
+B38Rq <- (mean(maleB38Rbase[1:10,3]) - mean(maleB38R10[1:10,3])) / B38RSE
+print(B38Rq)
+
+B38Rq1 <- abs(mean(maleB38Rbase[1:10,3]) - mean(maleB38R10[1:10,3])) / B38RSE
+B38Rq2 <- abs(mean(maleB38R10[1:10,3]) - mean(maleB38R30[1:10,3])) / B38RSE
+B38Rq3 <- abs(mean(maleB38R30[1:10,3]) - mean(male
+B38Rbase[1:10,3])) / B38RSE
+print(B38Rq1)
+print(B38Rq2)
+print(B38Rq3)
+
+
+
