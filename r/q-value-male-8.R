95608d660bfe8a709846bf21713b1afdb9ad9d76
kitada4010 <bnzz4010@yahoo.co.jp>
2018-11-05 11:05:47 +0900

    解析中

--- r/q-value-male-8.R
+++ r/q-value-male-8.R
@@ -26,77 +26,77 @@ maleB38R30=read.table("./male/B38R30.txt")
 
 ####開始########---------------------------------------------------------------------------------
 #B26R
-mean(maleB26Rbase[1:10,1])
-mean(maleB26R10[1:10,1])
-mean(maleB26R30[1:10,1])
+mean(maleB26Rbase[1:7,1])
+mean(maleB26R10[1:7,1])
+mean(maleB26R30[1:7,1])
 #B30R
-mean(maleB30Rbase[1:10,1])
-mean(maleB30R10[1:10,1])
-mean(maleB30R30[1:10,1])
+mean(maleB30Rbase[1:7,1])
+mean(maleB30R10[1:7,1])
+mean(maleB30R30[1:7,1])
 #B31R
-mean(maleB31Rbase[1:10,1])
-mean(maleB31R10[1:10,1])
-mean(maleB31R30[1:10,1])
+mean(maleB31Rbase[1:7,1])
+mean(maleB31R10[1:7,1])
+mean(maleB31R30[1:7,1])
 #B38R
-mean(maleB38Rbase[1:10,1])
-mean(maleB38R10[1:10,1])
-mean(maleB38R30[1:10,1])
+mean(maleB38Rbase[1:7,1])
+mean(maleB38R10[1:7,1])
+mean(maleB38R30[1:7,1])
 
 
 
 
 ######個体ごとの比較########------------------------------------------------------------------------------------------
 ##B26R##-------------------------
-B26RSMe <-(var(maleB26Rbase[1:10,1]) + var(maleB26R10[1:10,1]) + var(maleB26R30[1:10,1]))/3
+B26RSMe <-(var(maleB26Rbase[1:7,1]) + var(maleB26R10[1:7,1]) + var(maleB26R30[1:7,1]))/3
 B26RSE <- sqrt(B26RSMe/10)
-B26Rq <- (mean(maleB26R10[1:10,1]) - mean(maleB26Rbase[1:10,1])) / B26RSE
+B26Rq <- (mean(maleB26R10[1:7,1]) - mean(maleB26Rbase[1:7,1])) / B26RSE
 print(B26Rq)
 
-B26Rq1 <- abs(mean(maleB26Rbase[1:10,1]) - mean(maleB26R10[1:10,1])) / B26RSE
-B26Rq2 <- abs(mean(maleB26R10[1:10,1]) - mean(maleB26R30[1:10,1])) / B26RSE
-B26Rq3 <- abs(mean(maleB26R30[1:10,1]) - mean(maleB26Rbase[1:10,1])) / B26RSE
+B26Rq1 <- abs(mean(maleB26Rbase[1:7,1]) - mean(maleB26R10[1:7,1])) / B26RSE
+B26Rq2 <- abs(mean(maleB26R10[1:7,1]) - mean(maleB26R30[1:7,1])) / B26RSE
+B26Rq3 <- abs(mean(maleB26R30[1:7,1]) - mean(maleB26Rbase[1:7,1])) / B26RSE
 print(B26Rq1)
 print(B26Rq2)
 print(B26Rq3)
 
 
 ##B30R##-------------------------
-B30RSMe <-(var(maleB30Rbase[1:10,1]) + var(maleB30R10[1:10,1]) + var(maleB30R30[1:10,1]))/3
+B30RSMe <-(var(maleB30Rbase[1:7,1]) + var(maleB30R10[1:7,1]) + var(maleB30R30[1:7,1]))/3
 B30RSE <- sqrt(B30RSMe/10)
-B30Rq <- (mean(maleB30Rbase[1:10,1]) - mean(maleB30R10[1:10,1])) / B30RSE
+B30Rq <- (mean(maleB30Rbase[1:7,1]) - mean(maleB30R10[1:7,1])) / B30RSE
 print(B30Rq)
 
-B30Rq1 <- abs(mean(maleB30Rbase[1:10,1]) - mean(maleB30R10[1:10,1])) / B30RSE
-B30Rq2 <- abs(mean(maleB30R10[1:10,1]) - mean(maleB30R30[1:10,1])) / B30RSE
-B30Rq3 <- abs(mean(maleB30R30[1:10,1]) - mean(maleB30Rbase[1:10,1])) / B30RSE
+B30Rq1 <- abs(mean(maleB30Rbase[1:7,1]) - mean(maleB30R10[1:7,1])) / B30RSE
+B30Rq2 <- abs(mean(maleB30R10[1:7,1]) - mean(maleB30R30[1:7,1])) / B30RSE
+B30Rq3 <- abs(mean(maleB30R30[1:7,1]) - mean(maleB30Rbase[1:7,1])) / B30RSE
 print(B30Rq1)
 print(B30Rq2)
 print(B30Rq3)
 
 
 ##B31R##-------------------------
-B31RSMe <-(var(maleB31Rbase[1:10,1]) + var(maleB31R10[1:10,1]) + var(maleB31R30[1:10,1]))/3
+B31RSMe <-(var(maleB31Rbase[1:7,1]) + var(maleB31R10[1:7,1]) + var(maleB31R30[1:7,1]))/3
 B31RSE <- sqrt(B31RSMe/10)
-B31Rq <- (mean(maleB31Rbase[1:10,1]) - mean(maleB31R10[1:10,1])) / B31RSE
+B31Rq <- (mean(maleB31Rbase[1:7,1]) - mean(maleB31R10[1:7,1])) / B31RSE
 print(B31Rq)
 
-B31Rq1 <- abs(mean(maleB31Rbase[1:10,1]) - mean(maleB31R10[1:10,1])) / B31RSE
-B31Rq2 <- abs(mean(maleB31R10[1:10,1]) - mean(maleB31R30[1:10,1])) / B31RSE
-B31Rq3 <- abs(mean(maleB31R30[1:10,1]) - mean(maleB31Rbase[1:10,1])) / B31RSE
+B31Rq1 <- abs(mean(maleB31Rbase[1:7,1]) - mean(maleB31R10[1:7,1])) / B31RSE
+B31Rq2 <- abs(mean(maleB31R10[1:7,1]) - mean(maleB31R30[1:7,1])) / B31RSE
+B31Rq3 <- abs(mean(maleB31R30[1:7,1]) - mean(maleB31Rbase[1:7,1])) / B31RSE
 print(B31Rq1)
 print(B31Rq2)
 print(B31Rq3)
 
 
 ##B38R##-------------------------
-B38RSMe <-(var(maleB38Rbase[1:10,1]) + var(maleB38R10[1:10,1]) + var(maleB38R30[1:10,1]))/3
+B38RSMe <-(var(maleB38Rbase[1:7,1]) + var(maleB38R10[1:7,1]) + var(maleB38R30[1:7,1]))/3
 B38RSE <- sqrt(B38RSMe/10)
-B38Rq <- (mean(maleB38Rbase[1:10,1]) - mean(maleB38R10[1:10,1])) / B38RSE
+B38Rq <- (mean(maleB38Rbase[1:7,1]) - mean(maleB38R10[1:7,1])) / B38RSE
 print(B38Rq)
 
-B38Rq1 <- abs(mean(maleB38Rbase[1:10,1]) - mean(maleB38R10[1:10,1])) / B38RSE
-B38Rq2 <- abs(mean(maleB38R10[1:10,1]) - mean(maleB38R30[1:10,1])) / B38RSE
-B38Rq3 <- abs(mean(maleB38R30[1:10,1]) - mean(maleB38Rbase[1:10,1])) / B38RSE
+B38Rq1 <- abs(mean(maleB38Rbase[1:7,1]) - mean(maleB38R10[1:7,1])) / B38RSE
+B38Rq2 <- abs(mean(maleB38R10[1:7,1]) - mean(maleB38R30[1:7,1])) / B38RSE
+B38Rq3 <- abs(mean(maleB38R30[1:7,1]) - mean(maleB38Rbase[1:7,1])) / B38RSE
 print(B38Rq1)
 print(B38Rq2)
 print(B38Rq3)
@@ -109,76 +109,76 @@ print(B38Rq3)
 ####終了########----------------------------------------------------------------------------------------------
 #####平均の導出####----------------------------------
 #B26R
-mean(maleB26Rbase[1:10,2])
-mean(maleB26R10[1:10,2])
-mean(maleB26R30[1:10,2])
+mean(maleB26Rbase[1:7,2])
+mean(maleB26R10[1:7,2])
+mean(maleB26R30[1:7,2])
 #B30R
-mean(maleB30Rbase[1:10,2])
-mean(maleB30R10[1:10,2])
-mean(maleB30R30[1:10,2])
+mean(maleB30Rbase[1:7,2])
+mean(maleB30R10[1:7,2])
+mean(maleB30R30[1:7,2])
 #B31R
-mean(maleB31Rbase[1:10,2])
-mean(maleB31R10[1:10,2])
-mean(maleB31R30[1:10,2])
+mean(maleB31Rbase[1:7,2])
+mean(maleB31R10[1:7,2])
+mean(maleB31R30[1:7,2])
 #B38R
-mean(maleB38Rbase[1:10,2])
-mean(maleB38R10[1:10,2])
-mean(maleB38R30[1:10,2])
+mean(maleB38Rbase[1:7,2])
+mean(maleB38R10[1:7,2])
+mean(maleB38R30[1:7,2])
 
 
 
 ######個体ごとの比較########------------------------------------------------------------------------------------------
 ##B26R##-------------------------
-B26RSMe <-(var(maleB26Rbase[1:10,2]) + var(maleB26R10[1:10,2]) + var(maleB26R30[1:10,2]))/3
+B26RSMe <-(var(maleB26Rbase[1:7,2]) + var(maleB26R10[1:7,2]) + var(maleB26R30[1:7,2]))/3
 B26RSE <- sqrt(B26RSMe/10)
-B26Rq <- (mean(maleB26R10[1:10,2]) - mean(maleB26Rbase[1:10,2])) / B26RSE
+B26Rq <- (mean(maleB26R10[1:7,2]) - mean(maleB26Rbase[1:7,2])) / B26RSE
 print(B26Rq)
 
-B26Rq1 <- abs(mean(maleB26Rbase[1:10,2]) - mean(maleB26R10[1:10,2])) / B26RSE
-B26Rq2 <- abs(mean(maleB26R10[1:10,2]) - mean(maleB26R30[1:10,2])) / B26RSE
-B26Rq3 <- abs(mean(maleB26R30[1:10,2]) - mean(maleB26Rbase[1:10,2])) / B26RSE
+B26Rq1 <- abs(mean(maleB26Rbase[1:7,2]) - mean(maleB26R10[1:7,2])) / B26RSE
+B26Rq2 <- abs(mean(maleB26R10[1:7,2]) - mean(maleB26R30[1:7,2])) / B26RSE
+B26Rq3 <- abs(mean(maleB26R30[1:7,2]) - mean(maleB26Rbase[1:7,2])) / B26RSE
 print(B26Rq1)
 print(B26Rq2)
 print(B26Rq3)
 
 
 ##B30R##-------------------------
-B30RSMe <-(var(maleB30Rbase[1:10,2]) + var(maleB30R10[1:10,2]) + var(maleB30R30[1:10,2]))/3
+B30RSMe <-(var(maleB30Rbase[1:7,2]) + var(maleB30R10[1:7,2]) + var(maleB30R30[1:7,2]))/3
 B30RSE <- sqrt(B30RSMe/10)
-B30Rq <- (mean(maleB30Rbase[1:10,2]) - mean(maleB30R10[1:10,2])) / B30RSE
+B30Rq <- (mean(maleB30Rbase[1:7,2]) - mean(maleB30R10[1:7,2])) / B30RSE
 print(B30Rq)
 
-B30Rq1 <- abs(mean(maleB30Rbase[1:10,2]) - mean(maleB30R10[1:10,2])) / B30RSE
-B30Rq2 <- abs(mean(maleB30R10[1:10,2]) - mean(maleB30R30[1:10,2])) / B30RSE
-B30Rq3 <- abs(mean(maleB30R30[1:10,2]) - mean(maleB30Rbase[1:10,2])) / B30RSE
+B30Rq1 <- abs(mean(maleB30Rbase[1:7,2]) - mean(maleB30R10[1:7,2])) / B30RSE
+B30Rq2 <- abs(mean(maleB30R10[1:7,2]) - mean(maleB30R30[1:7,2])) / B30RSE
+B30Rq3 <- abs(mean(maleB30R30[1:7,2]) - mean(maleB30Rbase[1:7,2])) / B30RSE
 print(B30Rq1)
 print(B30Rq2)
 print(B30Rq3)
 
 
 ##B31R##-------------------------
-B31RSMe <-(var(maleB31Rbase[1:10,2]) + var(maleB31R10[1:10,2]) + var(maleB31R30[1:10,2]))/3
+B31RSMe <-(var(maleB31Rbase[1:7,2]) + var(maleB31R10[1:7,2]) + var(maleB31R30[1:7,2]))/3
 B31RSE <- sqrt(B31RSMe/10)
-B31Rq <- (mean(maleB31Rbase[1:10,2]) - mean(maleB31R10[1:10,2])) / B31RSE
+B31Rq <- (mean(maleB31Rbase[1:7,2]) - mean(maleB31R10[1:7,2])) / B31RSE
 print(B31Rq)
 
-B31Rq1 <- abs(mean(maleB31Rbase[1:10,2]) - mean(maleB31R10[1:10,2])) / B31RSE
-B31Rq2 <- abs(mean(maleB31R10[1:10,2]) - mean(maleB31R30[1:10,2])) / B31RSE
-B31Rq3 <- abs(mean(maleB31R30[1:10,2]) - mean(maleB31Rbase[1:10,2])) / B31RSE
+B31Rq1 <- abs(mean(maleB31Rbase[1:7,2]) - mean(maleB31R10[1:7,2])) / B31RSE
+B31Rq2 <- abs(mean(maleB31R10[1:7,2]) - mean(maleB31R30[1:7,2])) / B31RSE
+B31Rq3 <- abs(mean(maleB31R30[1:7,2]) - mean(maleB31Rbase[1:7,2])) / B31RSE
 print(B31Rq1)
 print(B31Rq2)
 print(B31Rq3)
 
 
 ##B38R##-------------------------
-B38RSMe <-(var(maleB38Rbase[1:10,2]) + var(maleB38R10[1:10,2]) + var(maleB38R30[1:10,2]))/3
+B38RSMe <-(var(maleB38Rbase[1:7,2]) + var(maleB38R10[1:7,2]) + var(maleB38R30[1:7,2]))/3
 B38RSE <- sqrt(B38RSMe/10)
-B38Rq <- (mean(maleB38Rbase[1:10,2]) - mean(maleB38R10[1:10,2])) / B38RSE
+B38Rq <- (mean(maleB38Rbase[1:7,2]) - mean(maleB38R10[1:7,2])) / B38RSE
 print(B38Rq)
 
-B38Rq1 <- abs(mean(maleB38Rbase[1:10,2]) - mean(maleB38R10[1:10,2])) / B38RSE
-B38Rq2 <- abs(mean(maleB38R10[1:10,2]) - mean(maleB38R30[1:10,2])) / B38RSE
-B38Rq3 <- abs(mean(maleB38R30[1:10,2]) - mean(maleB38Rbase[1:10,2])) / B38RSE
+B38Rq1 <- abs(mean(maleB38Rbase[1:7,2]) - mean(maleB38R10[1:7,2])) / B38RSE
+B38Rq2 <- abs(mean(maleB38R10[1:7,2]) - mean(maleB38R30[1:7,2])) / B38RSE
+B38Rq3 <- abs(mean(maleB38R30[1:7,2]) - mean(maleB38Rbase[1:7,2])) / B38RSE
 print(B38Rq1)
 print(B38Rq2)
 print(B38Rq3)
@@ -191,62 +191,62 @@ print(B38Rq3)
 ####中央########---------------------------------------------------------------------------------------------------------------
 #####平均の導出####----------------------------------
 #B26R
-mean(maleB26Rbase[1:10,3])
-mean(maleB26R10[1:10,3])
-mean(maleB26R30[1:10,3])
+mean(maleB26Rbase[1:7,3])
+mean(maleB26R10[1:7,3])
+mean(maleB26R30[1:7,3])
 #B30R
-mean(maleB30Rbase[1:10,3])
-mean(maleB30R10[1:10,3])
-mean(maleB30R30[1:10,3])
+mean(maleB30Rbase[1:7,3])
+mean(maleB30R10[1:7,3])
+mean(maleB30R30[1:7,3])
 #B31R
-mean(maleB31Rbase[1:10,3])
-mean(maleB31R10[1:10,3])
-mean(maleB31R30[1:10,3])
+mean(maleB31Rbase[1:7,3])
+mean(maleB31R10[1:7,3])
+mean(maleB31R30[1:7,3])
 #B38R
-mean(maleB38Rbase[1:10,3])
-mean(maleB38R10[1:10,3])
-mean(maleB38R30[1:10,3])
+mean(maleB38Rbase[1:7,3])
+mean(maleB38R10[1:7,3])
+mean(maleB38R30[1:7,3])
 
 
 
 ######個体ごとの比較########------------------------------------------------------------------------------------------
 ##B26R##-------------------------
-B26RSMe <-(var(maleB26Rbase[1:10,3]) + var(maleB26R10[1:10,3]) + var(maleB26R30[1:10,3]))/3
+B26RSMe <-(var(maleB26Rbase[1:7,3]) + var(maleB26R10[1:7,3]) + var(maleB26R30[1:7,3]))/3
 B26RSE <- sqrt(B26RSMe/10)
-B26Rq <- (mean(maleB26R10[1:10,3]) - mean(maleB26Rbase[1:10,3])) / B26RSE
+B26Rq <- (mean(maleB26R10[1:7,3]) - mean(maleB26Rbase[1:7,3])) / B26RSE
 print(B26Rq)
 
-B26Rq1 <- abs(mean(maleB26Rbase[1:10,3]) - mean(maleB26R10[1:10,3])) / B26RSE
-B26Rq2 <- abs(mean(maleB26R10[1:10,3]) - mean(maleB26R30[1:10,3])) / B26RSE
-B26Rq3 <- abs(mean(maleB26R30[1:10,3]) - mean(maleB26Rbase[1:10,3])) / B26RSE
+B26Rq1 <- abs(mean(maleB26Rbase[1:7,3]) - mean(maleB26R10[1:7,3])) / B26RSE
+B26Rq2 <- abs(mean(maleB26R10[1:7,3]) - mean(maleB26R30[1:7,3])) / B26RSE
+B26Rq3 <- abs(mean(maleB26R30[1:7,3]) - mean(maleB26Rbase[1:7,3])) / B26RSE
 print(B26Rq1)
 print(B26Rq2)
 print(B26Rq3)
 
 
 ##B30R##-------------------------
-B30RSMe <-(var(maleB30Rbase[1:10,3]) + var(maleB30R10[1:10,3]) + var(maleB30R30[1:10,3]))/3
+B30RSMe <-(var(maleB30Rbase[1:7,3]) + var(maleB30R10[1:7,3]) + var(maleB30R30[1:7,3]))/3
 B30RSE <- sqrt(B30RSMe/10)
-B30Rq <- (mean(maleB30Rbase[1:10,3]) - mean(maleB30R10[1:10,3])) / B30RSE
+B30Rq <- (mean(maleB30Rbase[1:7,3]) - mean(maleB30R10[1:7,3])) / B30RSE
 print(B30Rq)
 
-B30Rq1 <- abs(mean(maleB30Rbase[1:10,3]) - mean(maleB30R10[1:10,3])) / B30RSE
-B30Rq2 <- abs(mean(maleB30R10[1:10,3]) - mean(maleB30R30[1:10,3])) / B30RSE
-B30Rq3 <- abs(mean(maleB30R30[1:10,3]) - mean(maleB30Rbase[1:10,3])) / B30RSE
+B30Rq1 <- abs(mean(maleB30Rbase[1:7,3]) - mean(maleB30R10[1:7,3])) / B30RSE
+B30Rq2 <- abs(mean(maleB30R10[1:7,3]) - mean(maleB30R30[1:7,3])) / B30RSE
+B30Rq3 <- abs(mean(maleB30R30[1:7,3]) - mean(maleB30Rbase[1:7,3])) / B30RSE
 print(B30Rq1)
 print(B30Rq2)
 print(B30Rq3)
 
 
 ##B31R##-------------------------
-B31RSMe <-(var(maleB31Rbase[1:10,3]) + var(maleB31R10[1:10,3]) + var(maleB31R30[1:10,3]))/3
+B31RSMe <-(var(maleB31Rbase[1:7,3]) + var(maleB31R10[1:7,3]) + var(maleB31R30[1:7,3]))/3
 B31RSE <- sqrt(B31RSMe/10)
-B31Rq <- (mean(maleB31Rbase[1:10,3]) - mean(maleB31R10[1:10,3])) / B31RSE
+B31Rq <- (mean(maleB31Rbase[1:7,3]) - mean(maleB31R10[1:7,3])) / B31RSE
 print(B31Rq)
 
-B31Rq1 <- abs(mean(maleB31Rbase[1:10,3]) - mean(maleB31R10[1:10,3])) / B31RSE
-B31Rq2 <- abs(mean(maleB31R10[1:10,3]) - mean(maleB31R30[1:10,3])) / B31RSE
-B31Rq3 <- abs(mean(maleB31R30[1:10,3]) - mean(maleB31Rbase[1:10,3])) / B31RSE
+B31Rq1 <- abs(mean(maleB31Rbase[1:7,3]) - mean(maleB31R10[1:7,3])) / B31RSE
+B31Rq2 <- abs(mean(maleB31R10[1:7,3]) - mean(maleB31R30[1:7,3])) / B31RSE
+B31Rq3 <- abs(mean(maleB31R30[1:7,3]) - mean(maleB31Rbase[1:7,3])) / B31RSE
 print(B31Rq1)
 print(B31Rq2)
 print(B31Rq3)
@@ -254,14 +254,14 @@ print(B31Rq3)
 
 
 ##B38R##-------------------------
-B38RSMe <-(var(maleB38Rbase[1:10,3]) + var(maleB38R10[1:10,3]) + var(maleB38R30[1:10,3]))/3
+B38RSMe <-(var(maleB38Rbase[1:7,3]) + var(maleB38R10[1:7,3]) + var(maleB38R30[1:7,3]))/3
 B38RSE <- sqrt(B38RSMe/10)
-B38Rq <- (mean(maleB38Rbase[1:10,3]) - mean(maleB38R10[1:10,3])) / B38RSE
+B38Rq <- (mean(maleB38Rbase[1:7,3]) - mean(maleB38R10[1:7,3])) / B38RSE
 print(B38Rq)
 
-B38Rq1 <- abs(mean(maleB38Rbase[1:10,3]) - mean(maleB38R10[1:10,3])) / B38RSE
-B38Rq2 <- abs(mean(maleB38R10[1:10,3]) - mean(maleB38R30[1:10,3])) / B38RSE
-B38Rq3 <- abs(mean(maleB38R30[1:10,3]) - mean(maleB38Rbase[1:10,3])) / B38RSE
+B38Rq1 <- abs(mean(maleB38Rbase[1:7,3]) - mean(maleB38R10[1:7,3])) / B38RSE
+B38Rq2 <- abs(mean(maleB38R10[1:7,3]) - mean(maleB38R30[1:7,3])) / B38RSE
+B38Rq3 <- abs(mean(maleB38R30[1:7,3]) - mean(maleB38Rbase[1:7,3])) / B38RSE
 print(B38Rq1)
 print(B38Rq2)
 print(B38Rq3)
