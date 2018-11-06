f9ef45c706ea07a3a4dca67cd4424f91929a7911
kitada4010 <bnzz4010@yahoo.co.jp>
2018-11-06 13:08:39 +0900

    解析中

--- r/q-value-mix.R
+++ r/q-value-mix.R
@@ -50,7 +50,7 @@ mean(object30[,1])
 ######個体ごとの比較########------------------------------------------------------------------------------------------
 ##restraint##-------------------------
 restraintSMe <-(var(restraintbase[,1]) + var(restraint10[,1]) + var(restraint30[,1]))/3
-restraintSE <- sqrt(restraintSMe/10)
+restraintSE <- sqrt(restraintSMe/restraintbase[,1])
 restraintq <- (mean(restraint10[,1]) - mean(restraintbase[,1])) / restraintSE
 print(restraintq)
 
@@ -64,7 +64,7 @@ print(restraintq3)
 
 ##female##-------------------------
 femaleSMe <-(var(femalebase[,1]) + var(female10[,1]) + var(female30[,1]))/3
-femaleSE <- sqrt(femaleSMe/10)
+femaleSE <- sqrt(femaleSMe/length(femalebase[,1]))
 femaleq <- (mean(female10[,1]) - mean(femalebase[,1])) / femaleSE
 print(femaleq)
 
@@ -77,7 +77,7 @@ print(femaleq3)
 
 ##male##-------------------------
 maleSMe <-(var(malebase[,1]) + var(male10[,1]) + var(male30[,1]))/3
-maleSE <- sqrt(maleSMe/10)
+maleSE <- sqrt(maleSMe/length(malebase[,1]))
 maleq <- (mean(male10[,1]) - mean(malebase[,1])) / maleSE
 print(maleq)
 
@@ -90,7 +90,7 @@ print(maleq3)
 
 ##object##-------------------------
 objectSMe <-(var(objectbase[,1]) + var(object10[,1]) + var(object30[,1]))/3
-objectSE <- sqrt(objectSMe/10)
+objectSE <- sqrt(objectSMe/length(objectbase[,1]))
 objectq <- (mean(object10[,1]) - mean(objectbase[,1])) / objectSE
 print(objectq)
 
@@ -131,7 +131,7 @@ mean(object30[,2])
 ######個体ごとの比較########------------------------------------------------------------------------------------------
 ##restraint##-------------------------
 restraintSMe <-(var(restraintbase[,2]) + var(restraint10[,2]) + var(restraint30[,2]))/3
-restraintSE <- sqrt(restraintSMe/10)
+restraintSE <- sqrt(restraintSMe/length(restraintbase[,2]))
 restraintq <- (mean(restraint10[,2]) - mean(restraintbase[,2])) / restraintSE
 print(restraintq)
 
@@ -145,7 +145,7 @@ print(restraintq3)
 
 ##female##-------------------------
 femaleSMe <-(var(femalebase[,2]) + var(female10[,2]) + var(female30[,2]))/3
-femaleSE <- sqrt(femaleSMe/10)
+femaleSE <- sqrt(femaleSMe/length(femalebase[,2]))
 femaleq <- (mean(female10[,2]) - mean(femalebase[,2])) / femaleSE
 print(femaleq)
 
@@ -158,7 +158,7 @@ print(femaleq3)
 
 ##male##-------------------------
 maleSMe <-(var(malebase[,2]) + var(male10[,2]) + var(male30[,2]))/3
-maleSE <- sqrt(maleSMe/10)
+maleSE <- sqrt(maleSMe/length(malebase[,2]))
 maleq <- (mean(male10[,2]) - mean(malebase[,2])) / maleSE
 print(maleq)
 
@@ -171,7 +171,7 @@ print(maleq3)
 
 ##object##-------------------------
 objectSMe <-(var(objectbase[,2]) + var(object10[,2]) + var(object30[,2]))/3
-objectSE <- sqrt(objectSMe/10)
+objectSE <- sqrt(objectSMe/length(objectbase[,2]))
 objectq <- (mean(object10[,2]) - mean(objectbase[,2])) / objectSE
 print(objectq)
 
@@ -208,7 +208,7 @@ mean(object30[,3])
 ######個体ごとの比較########------------------------------------------------------------------------------------------
 ##restraint##-------------------------
 restraintSMe <-(var(restraintbase[,3]) + var(restraint10[,3]) + var(restraint30[,3]))/3
-restraintSE <- sqrt(restraintSMe/10)
+restraintSE <- sqrt(restraintSMe/length(restraintbase[,3]))
 restraintq <- (mean(restraint10[,3]) - mean(restraintbase[,3])) / restraintSE
 print(restraintq)
 
@@ -222,7 +222,7 @@ print(restraintq3)
 
 ##female##-------------------------
 femaleSMe <-(var(femalebase[,3]) + var(female10[,3]) + var(female30[,3]))/3
-femaleSE <- sqrt(femaleSMe/10)
+femaleSE <- sqrt(femaleSMe/length(femalebase[,3]))
 femaleq <- (mean(female10[,3]) - mean(femalebase[,3])) / femaleSE
 print(femaleq)
 
@@ -235,7 +235,7 @@ print(femaleq3)
 
 ##male##-------------------------
 maleSMe <-(var(malebase[,3]) + var(male10[,3]) + var(male30[,3]))/3
-maleSE <- sqrt(maleSMe/10)
+maleSE <- sqrt(maleSMe/length(malebase[,3]))
 maleq <- (mean(male10[,3]) - mean(malebase[,3])) / maleSE
 print(maleq)
 
@@ -248,7 +248,7 @@ print(maleq3)
 
 ##object##-------------------------
 objectSMe <-(var(objectbase[,3]) + var(object10[,3]) + var(object30[,3]))/3
-objectSE <- sqrt(objectSMe/10)
+objectSE <- sqrt(objectSMe/length(objectbase[,3]))
 objectq <- (mean(object10[,3]) - mean(objectbase[,3])) / objectSE
 print(objectq)
 
