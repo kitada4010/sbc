ff93ef6094a7355e096485f6f69d68f1640add7d
kitada4010 <bnzz4010@yahoo.co.jp>
2018-11-20 20:55:14 +0900

    ちょっと手直し

--- shell/directorychange.sh
+++ shell/directorychange.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POINT="/home/nodoka/txt-data"
 #POINT="~/txt-data"
-cd ${POINT}/restrant && ~/sbc/shell/filechange.sh
-cd ${POINT}/female && ~/sbc/shell/filechange.sh
-cd ${POINT}/male && ~/sbc/shell/filechange.sh
+cd ${POINT}/restrant && ~/sbc/shell/filechange.sh && 
+cd ${POINT}/female && ~/sbc/shell/filechange.sh && 
+cd ${POINT}/male && ~/sbc/shell/filechange.sh && 
 cd ${POINT}/object  ~/sbc/shell/filechange.sh 
