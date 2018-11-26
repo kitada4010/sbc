b0c98a39fa828c6f54fcfd2e6e3e739332516552
kitada4010 <bnzz4010@yahoo.co.jp>
2018-11-26 14:01:57 +0900

    解析中

--- shell/short-noise-comparison.sh
+++ shell/short-noise-comparison.sh
@@ -3,5 +3,5 @@
 POINT="/home/nodoka"
 while read line
 do
-    python ${POINT}/sbc/python/py/short-comparison.py ${line} noise
+    python ${POINT}/sbc/python/py/short-comparison.py ${line} -noise
 done < $1
