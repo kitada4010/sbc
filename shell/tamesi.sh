5f36ed7ebfea7c08cb1cf03da4577bfae403bb66
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 23:29:44 +0900

    同期

--- shell/tamesi.sh
+++ shell/tamesi.sh
@@ -5,11 +5,9 @@ PYENVPY="/home/nodoka/.pyenv/shims/python"
 KNN="/sbc/python/py/knn/"
 #rm $2
 #引数1 : エピソード
-if [ "$1" != "restraint" ] && [ "$1" != "male" ] && [ "$1" != "remale" ] && [ "$1" != "object" ]; then
-    echo HOWTO
-    echo $0 episode \(string\)
-    echo episode is restraint, male, female or object.
-    echo \(string\) is pattern. \(if necessary\)
-    exit 0
+echo ${1%%label.csv} 
+echo ${1%%label.csv}cut.csv
+if[ ${1%%label.csv}cut.csv = "$2" ];then
+    echo 判定
 fi
-echo eede
+	
