1795a15cf567419ecfd1b1c1bacc7d0d7a828ead
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 13:09:19 +0900

    同期

--- shell/make-feature.sh
+++ shell/make-feature.sh
@@ -9,15 +9,15 @@ KNN="/sbc/python/py/knn/"
 
 #while read line
 for line in *time*; do
-
+filename=line
     while read timedata
     do
-	${PYENVPY} $HOME${POINT}${KNN}datacut.py $timedata $1 ${line:0:3}-H${line:3:1} 
-	${PYENVPY} $HOME${POINT}${KNN}psdatacut.py $timedata $1 ${line:0:3}-H${line:3:1}
-	cut -f 150- -d "," $1-${line:0:3}-${timedata/[   ]/-}-cut.csv >  $1-${line:0:3}-H${line:3:1}-${timedata/[   ]/-}-150-highpass.csv
-	cut -f 150- -d "," $1-${line:0:3}-${timedata/[   ]/-}-pscut.csv >  $1-${line:0:3}-H${line:3:1}-${timedata/[   ]/-}-150-pshighpass.csv
-	${PYENVPY} $HOME{POINT}${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[   ]/-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
-	${PYENVPY} $HOME{POINT}${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[   ]/-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
+	echo ${PYENVPY} $HOME${POINT}${KNN}datacut.py $timedata $1 ${line:0:3}-H${line:3:1} 
+	echo ${PYENVPY} $HOME${POINT}${KNN}psdatacut.py $timedata $1 ${line:0:3}-H${line:3:1}
+	echo cut -f 150- -d "," $1-${line:0:3}-${timedata/[ ]/-}-cut.csv >  $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-highpass.csv
+	echo cut -f 150- -d "," $1-${line:0:3}-${timedata/[ ]/-}-pscut.csv >  $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-pshighpass.csv
+	echo ${PYENVPY} $HOME{POINT}${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
+	echo ${PYENVPY} $HOME{POINT}${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
     done < ${line}
 
 done
