0d1c8bce9377d63dba2b13b316dfc03f9697ffc1
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 14:12:51 +0900

    同期

--- shell/make-feature.sh
+++ shell/make-feature.sh
@@ -12,13 +12,13 @@ for line in *time*; do
 filename=$line
     while read timedata
     do
-	echo $timedata
-	echo ${PYENVPY} $HOME${KNN}datacut.py $timedata $1 ${line:0:3}-H${line:3:1} 
-	echo ${PYENVPY} $HOME${KNN}psdatacut.py $timedata $1 ${line:0:3}-H${line:3:1}
-	echo cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-cut.csv | tee  $1-${line:0:3}-H${line3:1}-${timedata/   /-}-150-highpass.csv
-	echo cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-pscut.csv | tee  $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-150-pshighpass.csv
-	echo ${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
-	echo ${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
+	$timedata
+	${PYENVPY} $HOME${KNN}datacut.py $timedata $1 ${line:0:3}-H${line:3:1} 
+	${PYENVPY} $HOME${KNN}psdatacut.py $timedata $1 ${line:0:3}-H${line:3:1}
+	cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-cut.csv | tee  $1-${line:0:3}-H${line3:1}-${timedata/   /-}-150-highpass.csv
+	cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-pscut.csv | tee  $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-150-pshighpass.csv
+	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
+	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/   /-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
 	#echo cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/[ 	][	  ]*/-}-cut.csv | tee  $1-${line:0:3}-H${line3:1}-${timedata/[ 	][	 ]/-}-150-highpass.csv
 	#echo cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/[ 	][	 ]*/-}-pscut.csv | tee  $1-${line:0:3}-H${line:3:1}-${timedata/[ 	][	 ]*/-}-150-pshighpass.csv
 	#echo ${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ 	][	 ]*/-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
