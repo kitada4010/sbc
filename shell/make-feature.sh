7c4da0a65c035a249e38b07da190a07b5cb1824a
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 13:37:05 +0900

    同期

--- shell/make-feature.sh
+++ shell/make-feature.sh
@@ -15,10 +15,10 @@ for line in *time*; do
 	#filename=line
 	${PYENVPY} $HOME${KNN}datacut.py $timedata $1 ${line:0:3}-H${line:3:1} 
 	${PYENVPY} $HOME${KNN}psdatacut.py $timedata $1 ${line:0:3}-H${line:3:1}
-	cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-cut.csv | tee  $1-${line:0:3}-H${line3:1}-${timedata/[ ]/-}-150-highpass.csv
-	cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-pscut.csv | tee  $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-pshighpass.csv
-	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
-	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
+	cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/  */-}-cut.csv | tee  $1-${line:0:3}-H${line3:1}-${timedata/  */-}-150-highpass.csv
+	cut -f 150- -d "," $1-${line:0:3}-H${line:3:1}-${timedata/  */-}-pscut.csv | tee  $1-${line:0:3}-H${line:3:1}-${timedata/  */-}-150-pshighpass.csv
+	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/  */-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
+	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/  */-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
     done < ${line}
 
 done
