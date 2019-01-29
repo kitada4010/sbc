d54b8d0221309a982e5376beab3df72df370428b
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 19:18:23 +0900

    d

--- shell/summarylable.sh
+++ shell/summarylable.sh
@@ -4,8 +4,9 @@ POINT="/home/nodoka"
 PYENVPY="/home/nodoka/.pyenv/shims/python"
 #while read line
 for line in *$1*; do
+file=$line
     while read filename
     do
-	${POINT}/sbc/c/label $filename $2 ${line:0:4}
-    done < $line
+	${POINT}/sbc/c/label $filename $2 ${file:0:4}
+    done < $file
 done
