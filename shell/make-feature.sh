7cbc50812d46eaa740b314be4da4d6f99989ddfe
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 10:47:34 +0900

    ubuntu移行

--- shell/make-feature.sh
+++ shell/make-feature.sh
@@ -3,8 +3,11 @@
 POINT="/home/nodoka"
 PYENVPY="/home/nodoka/.pyenv/shims/python"
 #rm $2
+#引数1 : ファイル名に共通するtime
+
 #while read line
 for line in *$1*; do
     echo $line 
     echo ${line:0:3}-H${line:3:1}
+    echo $line | cut -f 2 -d "-"
 done
