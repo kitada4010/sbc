af0a71f93a7a7b4b97a641d31a4e0e5fee58ca0b
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 19:50:56 +0900

    to ubuntu

--- shell/summarylable.sh
+++ shell/summarylable.sh
@@ -7,6 +7,6 @@ for line in *$1*; do
 file=$line
     while read filename
     do
-	${POINT}/sbc/c/label $filename $2 ${file:0:4}
+	${POINT}/sbc/c/label $filename $2 ${file:0:3}-H${file:3:1}
     done < ${file}
 done
