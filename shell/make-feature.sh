1c804dab7141a9f3e8f2950ab0157beb4c410462
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 13:26:14 +0900

    df

--- shell/make-feature.sh
+++ shell/make-feature.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #POINT="/home2/nodoka"
-POINT=""
+#POINT=""
 PYENVPY="/home/nodoka/.pyenv/shims/python"
 KNN="/sbc/python/py/knn/"
 #rm $2
@@ -13,12 +13,12 @@ for line in *time*; do
     while read timedata
     do
 	filename=line
-	${PYENVPY} $HOME${POINT}${KNN}datacut.py $timedata $1 ${line:0:3}-H${line:3:1} 
-	${PYENVPY} $HOME${POINT}${KNN}psdatacut.py $timedata $1 ${line:0:3}-H${line:3:1}
+	${PYENVPY} $HOME${KNN}datacut.py $timedata $1 ${line:0:3}-H${line:3:1} 
+	${PYENVPY} $HOME${KNN}psdatacut.py $timedata $1 ${line:0:3}-H${line:3:1}
 	cut -f 150- -d "," $1-${line:0:3}-${timedata/[ ]/-}-cut.csv | tee  $1-${filename:0:3}-H${filename3:1}-${timedata/[ ]/-}-150-highpass.csv
 	cut -f 150- -d "," $1-${line:0:3}-${timedata/[ ]/-}-pscut.csv | tee  $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-pshighpass.csv
-	${PYENVPY} $HOME{POINT}${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
-	${PYENVPY} $HOME{POINT}${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
+	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-150-highpass.csv $1 ${line:0:3}-H${line:3:1} 
+	${PYENVPY} $HOME${KNN}normalize.py $timedata 150 $1-${line:0:3}-H${line:3:1}-${timedata/[ ]/-}-ps150-highpass.csv $1 ${line:0:3}-H${line:3:1}ps 
     done < ${line}
 
 done
