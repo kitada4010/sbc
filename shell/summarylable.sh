ef4d9e949c9f5dce88e542379b376e8e7d4fcb5f
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-29 18:54:00 +0900

    label change collect

--- shell/summarylable.sh
+++ shell/summarylable.sh
@@ -4,5 +4,5 @@ POINT="/home/nodoka/sbc/shell/"
 PYENVPY="/home/nodoka/.pyenv/shims/python"
 #while read line
 for line in *$1*; do
-    ${POINT}lable.sh $line $2
+    ${POINT}label.sh $line $2
 done
