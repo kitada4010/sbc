2b35df33299278aee305dd714c04483ffc16d3c3
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-30 12:50:23 +0900

    同期

--- shell/specrm.sh
+++ shell/specrm.sh
@@ -6,7 +6,7 @@ KNN="/sbc/python/py/knn/"
 #rm $2
 #引数1 : エピソード
 for line in *$1*; do
-    if [ ! -e "${line%%${1}.csv}pscut.csv" ]; then
-	rm ${line%%label.csv}*
+    if [ ! -e "${line%%${1}.csv}$2.csv" ]; then
+	rm ${line%%${1}.csv}*
     fi
 done
