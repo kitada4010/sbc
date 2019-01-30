b7394b19b1c341527992367e60411f1f99cb8c9d
kitada4010 <bnzz4010@yahoo.co.jp>
2019-01-30 12:46:38 +0900

    同期

--- shell/specrm.sh
+++ shell/specrm.sh
@@ -5,8 +5,8 @@ PYENVPY="/home/nodoka/.pyenv/shims/python"
 KNN="/sbc/python/py/knn/"
 #rm $2
 #引数1 : エピソード
-for line in *label*; do
-    if [ ! -e "${line%%label.csv}pscut.csv" ]; then
+for line in *$1*; do
+    if [ ! -e "${line%%${1}.csv}pscut.csv" ]; then
 	rm ${line%%label.csv}*
     fi
 done
