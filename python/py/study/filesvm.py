008ef78d730f462d33095e0bddc61fca7e275b9d
kitada4010 <bnzz4010@yahoo.co.jp>
2019-02-06 09:29:06 +0900

    同期

--- python/py/study/filesvm.py
+++ python/py/study/filesvm.py
@@ -75,14 +75,14 @@ result_train_mean = result_train.mean(axis=0)
 result_train_std = result_train.std(axis=0)
 f = open(sys.argv[3]+"-train.tex",'w')
 f.write("accuracy:{:.4f} $\\pm$ {:.4f}\n".format(accuracy_train.mean(axis=0), accuracy_train.std(axis=0)))
-f.write("\\begin{table}[tbp]\n")  
-f.write('  \\begin{center}\n')
-f.write('    \caption{$SVM$による神経活動テストデータ判定の混同行列}\n')
-f.write('    \label{knnconfusiontest}\n')
+# f.write("\\begin{table}[tbp]\n")  
+# f.write('  \\begin{center}\n')
+# f.write('    \caption{$SVM$による神経活動テストデータ判定の混同行列}\n')
+# f.write('    \label{knnconfusiontest}\n')
 f.write('    \\begin{tabular}{| l | l | l | l | l | l | l | l |}\hline \n')
-f.write('      \multicolumn{2}{|c|}{}& \multicolumn{6}{c|}{予測結果}\\\\ \hline \n')
+f.write('      \multicolumn{2}{|c|}{}& \multicolumn{6}{c|}{自動判定結果 (平均 $\pm$ 標準偏差 [\%])}\\\\ \hline \n')
 f.write('      && ベース波形 & リップル波形 & ノイズ & リップル & ノイズ & リップル \\\\ \n')
-f.write('      &&  &  &  & ・ベース & ・ベース & ・ノイズ \\\\ \cline{{2-8}} \n')
+f.write('      &&  &  &  & ・ベース & ・ベース & ・ノイズ \\\\ \cline{2-8} \n')
 f.write('      実&ベース波形 & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
 .format(result_train_mean[0][0], result_train_std[0][0], result_train_mean[0][1], result_train_std[0][1], result_train_mean[0][2], result_train_std[0][2], result_train_mean[0][3], result_train_std[0][3], result_train_mean[0][4], result_train_std[0][4], result_train_mean[0][5], result_train_std[0][5]))
 f.write('      際& リップル波形 & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
@@ -96,8 +96,8 @@ f.write('      ベ& ノイズ・ベース & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$
 f.write('      ル& リップル・ノイズ & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \hline \n' 
 .format(result_train_mean[5][0], result_train_std[5][0], result_train_mean[5][1], result_train_std[5][1], result_train_mean[5][2], result_train_std[5][2], result_train_mean[5][3], result_train_std[5][3], result_train_mean[5][4], result_train_std[5][4], result_train_mean[5][5], result_train_std[5][5]))
 f.write('    \end{tabular} \n')
-f.write('  \end{center} \n')
-f.write('\end{table} \n')
+#f.write('  \end{center} \n')
+#f.write('\end{table} \n')
 f.close()
 
 
@@ -113,20 +113,20 @@ f.write("accuracy:{:.4f} $\\pm$ {:.4f}\n".format(accuracy.mean(axis=0), accuracy
 #f.write('    \caption{$SVM$による神経活動テストデータ判定の混同行列}\n')
 #f.write('    \label{knnconfusiontest}\n')
 f.write('    \\begin{tabular}{| l | l | l | l | l | l | l | l |}\hline \n')
-f.write('      \multicolumn{2}{|c|}{}& \multicolumn{6}{c|}{予測結果}\\\\ \hline \n')
+f.write('      \multicolumn{2}{|c|}{}& \multicolumn{6}{c|}{自動判定結果 (平均 $\pm$ 標準偏差 [\%])}\\\\ \hline \n')
 f.write('      && ベース波形 & リップル波形 & ノイズ & リップル & ノイズ & リップル \\\\ \n')
-f.write('      &&  &  &  & ・ベース & ・ベース & ・ノイズ \\\\ \cline{{2-8}} \n')
+f.write('      &&  &  &  & ・ベース & ・ベース & ・ノイズ \\\\ \cline{2-8} \n')
 f.write('      実&ベース波形 & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
 .format(result_mean[0][0], result_std[0][0], result_mean[0][1], result_std[0][1], result_mean[0][2], result_std[0][2], result_mean[0][3], result_std[0][3], result_mean[0][4], result_std[0][4], result_mean[0][5], result_std[0][5]))
 f.write('      際& リップル波形 & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
 .format(result_mean[1][0], result_std[1][0], result_mean[1][1], result_std[1][1], result_mean[1][2], result_std[1][2], result_mean[1][3], result_std[1][3], result_mean[1][4], result_std[1][4], result_mean[1][5], result_std[1][5]))
 f.write('      の& ノイズ & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
       .format(result_mean[2][0], result_std[2][0], result_mean[2][1], result_std[2][1], result_mean[2][2], result_std[2][2], result_mean[2][3], result_std[2][3], result_mean[2][4], result_std[2][4], result_mean[2][5], result_std[2][5]))
-f.write('      分& リップル・ベース & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
+f.write('      ラ& リップル・ベース & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
 .format(result_mean[3][0], result_std[3][0], result_mean[3][1], result_std[3][1], result_mean[3][2], result_std[3][2], result_mean[3][3], result_std[3][3], result_mean[3][4], result_std[3][4], result_mean[3][5], result_std[3][5]))
-f.write('      類& ノイズ・ベース & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
+f.write('      ベ& ノイズ・ベース & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \cline{{2-8}} \n' 
 .format(result_mean[4][0], result_std[4][0], result_mean[4][1], result_std[4][1], result_mean[4][2], result_std[4][2], result_mean[4][3], result_std[4][3], result_mean[4][4], result_std[4][4], result_mean[4][5], result_std[4][5]))
-f.write('      & リップル・ノイズ & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \hline \n' 
+f.write('      ル& リップル・ノイズ & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f} & {:.1f} $\\pm$ {:.1f}  \\\\ \hline \n' 
 .format(result_mean[5][0], result_std[5][0], result_mean[5][1], result_std[5][1], result_mean[5][2], result_std[5][2], result_mean[5][3], result_std[5][3], result_mean[5][4], result_std[5][4], result_mean[5][5], result_std[5][5]))
 f.write('    \end{tabular} \n')
 #f.write('  \end{center} \n')
