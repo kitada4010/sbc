4955d6ddac34c580b83f19311acd9dcfe4fb3f26
kitada4010 <bnzz4010@yahoo.co.jp>
2019-07-14 18:32:37 +0900

    同期

--- python/py/save/knn_matrix.py
+++ python/py/save/knn_matrix.py
@@ -29,22 +29,24 @@ Y = np.loadtxt(sys.argv[2],delimiter=",")
 
 #with open('/home/hera/nodoka/home2/nodoka/spike-data/25kHz-data/B39 Rd.pickle', mode='rb') as fp:
 with open(sys.argv[3], mode='rb') as st:
-    model = pickle.load(st)
+    knc = pickle.load(st)
 
 
 
 k=3
 roop = 100
 #result = np.empty((6,6),int)
-result = []
-Y_pred = knc.predict
+#result = []
+Y_pred = knc.predict(X)
 
 result = np.array(confusion_matrix(Y,Y_pred), dtype = 'float')
+result_sum = result.sum(axis=1)
+for x in range(6):
+    result[x,:] = 100*result[x,:]/result_sum[x]
 
-
-f = open(sys.argv[3],'w')
+f = open(sys.argv[4],'w')
 f.write("accuracy:{:.4f} \n"
-.format(knc.sorce(X,Y))
+.format(knc.score(X,Y)))
 #f.write("\\begin{table}[tbp]\# n")  
 # f.write('  \\begin{center}\n')
 # f.write('    \caption{$SVM$による神経活動テストデータ判定の混同行列}\n')
@@ -53,18 +55,18 @@ f.write('\begin{tabular}{| c | c | r | r | r | r | r | r | r |}\hline \n')
 f.write('  \multicolumn{3}{|c|}{}& \multicolumn{6}{c|}{識別率 ( 平均 $\pm$ 標準偏差 [\%])}\\\\ \hline \n')
 f.write('  \multicolumn{3}{|c|}{}& \multicolumn{3}{c|}{経験前} & \multicolumn{3}{c|}{経験後}\\\\ \hline \n')
 f.write('  &&& ベース波形 & リップル波形 & リップル & ベース波形 & リップル波形 & リップル \\\\ \n')
-f.write('  &&&  &  &  ・ベース&  &  & ・ベース \\\\ \cline{2-9} \n')
-f.write('  & 経 &ベース波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{3-9} \n'
+f.write('  &&&  &  &  ・ベース&  &  & ・ベース \\\\ \cline{{2-9}} \n')
+f.write('  & 経 &ベース波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{{3-9}} \n'
 .format(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5]))
-f.write('  入& 験 & リップル波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{3-9} \n'
+f.write('  入& 験 & リップル波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{{3-9}} \n'
 .format(result[1][0], result[1][1], result[1][2], result[1][3], result[1][4], result[1][5]))
-f.write('  力& 前 &リップル・ベース & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{3-9} \n'
+f.write('  力& 前 &リップル・ベース & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{{3-9}} \n'
 .format(result[2][0], result[2][1], result[2][2], result[2][3], result[2][4], result[2][5]))
-f.write('  波& 経 &  ベース波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{3-9} \n'
+f.write('  波& 経 &  ベース波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{{3-9}} \n'
 .format(result[3][0], result[3][1], result[3][2], result[3][3], result[3][4], result[3][5]))
-f.write('  形& 験 &  リップル波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{3-9} \n'
+f.write('  形& 験 &  リップル波形 & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{{3-9}} \n'
 .format(result[4][0], result[4][1], result[4][2], result[4][3], result[4][4], result[4][5]))
-f.write('  & 後 &  リップル・ベース & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{3-9} \n'
+f.write('  & 後 &  リップル・ベース & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f} & {:.1f}  \\\\ \cline{{3-9}} \n'
 .format(result[5][0], result[5][1], result[5][2], result[5][3], result[5][4], result[5][5]))
 f.write('\end{tabular} ')
 
