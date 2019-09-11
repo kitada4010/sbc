dca83812243f248cc76c89b1d9eb214f4d405de1
kitada4010 <bnzz4010@yahoo.co.jp>
2019-09-11 18:36:10 +0900

    修復

---
+++ python/py/cut-time/cut-time-label.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+#エクセルファイルの変換を目指して
+import pandas as pd
+import numpy as np
+import sys
+import os.path
+
+
+name, ext = os.path.splitext(sys.argv[1])
+file = pd.ExcelFile(sys.argv[1])
+time_file = open(name+'_time_data.csv', 'w')
+label_file = open(name+'_label_data.csv', 'w')
+
+sheet_df = file.parse(file.sheet_names, header=None)
+all_df = pd.DataFrame()
+
+sheet_names = file.sheet_names
+for i, name in enumerate(sheet_names):
+    sheet_df[i] = file.parse(name)
+    start_time = sheet_df[i]['Unnamed: 1'][(np.where(sheet_df[i]['INFORMATION']=="START")[0][0])]
+    end_time = (np.where(sheet_df[i]['INFORMATION']=="START")[0][1]/25000)+ start_time
+    print("{:.5f},{:.5f}".format(start_time, end_time), file=time_file)
+    print("{:.5f},{:.5f},{:.5f},{:.5f}".format(start_time-0.2, end_time+0.2, start_time, end_time), file=label_file)
+
+file.close()
+time_file.close()
+label_file.close()
