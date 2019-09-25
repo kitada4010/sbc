653d8e160d8ef02fee3a624f420bab8fee5f70e2
kitada4010 <bnzz4010@yahoo.co.jp>
2019-09-25 19:07:52 +0900

    同期

--- shell/or-label.sh
+++ shell/or-label.sh
@@ -2,10 +2,12 @@
 #POINT="/home2/nodoka"
 POINT="/home/nodoka"
 #PYENVPY="/home/nodoka/.pyenv/shims/python"
+# 引数1 label時間ファイル
+# 引数2 エピソード-個体名-(HR or HL)
 
 while read line
 do
-    ${POINT}/sbc/c/or-label $line $2 
+    ${POINT}/sbc/c/or-label $line 1 $2 
 #    if [ $3 = "3" ]; then
 #	sed -i "s/1/5/g" B39-${line/[    ]/-}-label.csv
 #	sed -i "s/3/5/g" B39-${line/[    ]/-}-label.csv
