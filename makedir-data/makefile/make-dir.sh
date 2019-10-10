41ceb233116e68fbf109b676bc596ffe4e6e568a
KITADA Nodoka <b009vb@tesla3.ds.sci.yamaguchi-u.ac.jp>
2019-10-10 17:18:34 +0900

    同期

--- makedir-data/makefile/make-dir.sh
+++ makedir-data/makefile/make-dir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dirs=`find ./individual/*.txt`
-TIMEDATAPATH="/home/nodoka/win/ubuntu/kuttuketime-label"
-#TIMEDATAPATH="/mnt/export1/st9/b009vb/yuni/kuttuketime-label"
+#TIMEDATAPATH="/home/nodoka/win/ubuntu/kuttuketime-label"
+TIMEDATAPATH="/mnt/export1/st9/b009vb/yuni/kuttuketime-label"
 #HZCUTS="3072"
 #HZCUTL="2048"
 HZCUT="1024"
@@ -13,15 +13,21 @@ MODE="label"
 
 for dir in $dirs;
 do
+    echo $dir
+    echo "実行中"
     file=${dir##*/}
     echo ${file%.*}
     episodehz=${file%.*}
     [ -d $episodehz ] || mkdir $episodehz
      while read line
      do
+	 echo $line
+	 echo "実行中"
 	 [ -d $episodehz/$line ] || mkdir $episodehz/$line
 	 while read indivi
 	 do
+	 echo $indivi
+	 echo "実行中"
 	     [ -d $episodehz/$line/$inidivi ] || mkdir $episodehz/$line/$indivi
 	     
 	     #それぞれのファイルで実行したいシェルスクリプトの記入 jikkou.txtに実行コマンドを書き込んでいく
@@ -55,24 +61,30 @@ do
 	     chmod +x $episodehz/$line/$indivi/conduct.sh
 	     make -C $episodehz/$line/$indivi/
 	     rsync $episodehz/$line/$indivi/${file%-*}-$line-$indivi-$MODE.csv $episodehz/$line/
-	     
-	     	     	     	     
+
+	 echo $indivi
+	 echo "終了"	     
+	 
 	 done < $dir
-	 echo ~/sbc/shell/group.sh  $MODE.csv ${file%-*}-$line-$MODE.csv > jikkou.txt     
-	 cat shell-head.txt jikkou.txt > $episodehz/$line/$indivi/conduct.sh
+	 echo $HOME/sbc/shell/group.sh  $MODE.csv ${file%-*}-$line-$MODE.csv > jikkou.txt     
+	 cat shell-head.txt jikkou.txt > $episodehz/$line/conduct.sh
+	 chmod +x $episodehz/$line/conduct.sh
 	 rsync ./okiba/Makefile  $episodehz/$line/
 	 make -C $episodehz/$line/
 	 rsync $episodehz/$line/${file%-*}-$line-$indivi$MODE.csv $episodehz/
 	 
+	 echo $line
+	 echo "終了"	     
      done < timing.txt
      
-     echo ~/sbc/shell/group.sh -$MODE.csv $episodehz-$MODE.csv > jikkou.txt
+     echo $HOME/sbc/shell/group.sh -$MODE.csv $episodehz-$MODE.csv > jikkou.txt
      cat shell-head.txt jikkou.txt > $episodehz/conduct.sh
      chmod +x $episodehz/conduct.sh
      rsync ./okiba/Makefile  $episodehz/
      make -C $episodehz/
      rsync $episodehz/$episodehz-$MODE*.csv ./
-          
+     echo $dir
+     echo "終了"	     
 done
 
 ~/sbc/shell/group.sh  $MODE.csv $MODE.csv  
