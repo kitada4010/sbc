3a096bc01aa326994fe29a3a27a5b08ef424460d
kitada4010 <bnzz4010@yahoo.co.jp>
2020-05-19 11:13:51 +0900

    d

--- makedir-data/all-make-jensen-shannon/make-dir.sh
+++ makedir-data/all-make-jensen-shannon/make-dir.sh
@@ -18,9 +18,9 @@ PROG="/sbc/python/py/pattern/jensen-shannon-2.1.py"
 
 #パラメータの範囲指定
 TIME_RANGE_S="1"
-TIME_RANGE_E="100"
+TIME_RANGE_E="101"
 PATTERN_RANGE_S="1"
-PATTERN_RANGE_E="50"
+PATTERN_RANGE_E="51"
 SKIP="1"
 TOP_PATTERN="20"
 #入力ファイル指定 2:経験前, 3:経験後0~10分間, 4:経験後10~20分間, 5:経験後20~30分間
@@ -52,25 +52,24 @@ do
 	[ -f jikkou.txt ] && rm jikkou.txt
 	#divergence.txt を削除
 	[ -f $episodehz/$indivi/divergence.txt ] && rm $episodehz/$indivi/divergence.txt
-	[ -f $episodehz/$indivi/data-ab.txt ] && rm $episodehz/$indivi/data-ab.txt 
+	[ -f $episodehz/$indivi/data-ab.txt ] && rm $episodehz/$indivi/data-ab.txt
+	[ -f $episodehz/$indivi/count-data.txt ] && rm $episodehz/$indivi/count-data.txt 
 	#divergence-old.txt があれば置き換え
 	#[ -f $episodehz/$indivi/divergence.txt ] && mv $episodehz/$indivi/divergence.txt  $episode/$indivi/divergence-old.txt
 	#[ -f $episodehz/$indivi/data-ab.txt ] && mv $episodehz/$indivi/data-ab.txt  $episode/$indivi/data-ab-old.txt
 	#divergence-old.txt を削除
 	#[ -f $episodehz/$indivi/divergence-old.txt ] && rm $episodehz/$indivi/divergence-old.txt
 	#[ -f $episodehz/$indivi/data-ab-old.txt ] && rm $episodehz/$indivi/data-ab-old.txt  
-	
 	#conduct.sh(実行文をまとめたシェルスクリプト)の作成
-	for ((i=0; i<TIME_RANGE_E; i+SKIP))
+	for ((i=1; i<TIME_RANGE_E; i+=SKIP))
 	do
 	    # 実行文の出力
             echo $PYTHON $HOME$PROG  $LABELDATAPATH/${file%.*}/${indivi}/${indivi%-*}-${file%-*}-${indivi##*H}$EP_A.xlsx  $LABELDATAPATH/${file%.*}/${indivi}/${indivi%-*}-${file%-*}-${indivi##*H}$EP_B.xlsx $i $i $PATTERN_RANGE_S  $PATTERN_RANGE_E $SKIP $TOP_PATTERN >> jikkou.txt
 	done
-	
 	rsync ./okiba/Makefile $episodehz/$indivi/
 	cat shell-head.txt jikkou.txt > $episodehz/$indivi/conduct.sh
 	chmod +x $episodehz/$indivi/conduct.sh
-	make conduct -C $episodehz/$indivi/	
+	#make conduct -C $episodehz/$indivi/	
 #CONDUCT
 	
 	
