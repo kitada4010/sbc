0ce1378412f3f21848a5ffe65cfcef21c6151714
kitada4010 <bnzz4010@yahoo.co.jp>
2020-05-28 15:41:03 +0900

    remake jensen

--- python/py/pattern/jensen-shannon-2.2.py
+++ python/py/pattern/jensen-shannon-2.2.py
@@ -166,7 +166,7 @@ def inspect(time_leng, pattern_leng, count_data):
 
 
 
-    del pattern_dict1, pattern_dict2, sum_dict, probability1, probability2,  print_probability1, print_probability2, print_count1, print_count2, print_kullback, top_dict, print_pattern, leng, psth, start_number, end_number, sig1, i, l, k, x, w
+    del pattern_dict1, pattern_dict2, sum_dict, probability1, probability2, top_dict, leng, psth, start_number, end_number, sig1, i, l, k, x, w
     return pattern_information, sum_pattern1, sum_pattern2
 
 parameter1_start = int(sys.argv[3])
